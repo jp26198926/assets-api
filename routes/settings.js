8f5558564771b2ff378a450ab904294f24a642f5
Jaypee Hindang <jp@upliftcodecamp.com>
2025-04-18 22:37:14 +1000

    feat: added app settings

--- routes/settings.js
+++ routes/settings.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const Settings = require("../models/Settings");
-const auth = require("../middleware/auth");
+const { auth } = require("../middleware/auth");
 
 // Get settings
 router.get("/", async (req, res) => {
